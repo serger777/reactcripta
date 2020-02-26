0aad345598545d946309622778646a396df72db5
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2020-02-26 17:32:54 +0300

    Updates

---
+++ precache-manifest.dabf4fa34b3ea28028af68d9ddf2b6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54158c5f02f5391a52da294f6c8f4b9b",
+    "url": "/reactcripta/index.html"
+  },
+  {
+    "revision": "1f81c08eea573d63a2e0",
+    "url": "/reactcripta/static/css/main.086a47c0.chunk.css"
+  },
+  {
+    "revision": "7890a3ed55c2d981ab54",
+    "url": "/reactcripta/static/js/2.6c5bd136.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/reactcripta/static/js/2.6c5bd136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f81c08eea573d63a2e0",
+    "url": "/reactcripta/static/js/main.d7978b89.chunk.js"
+  },
+  {
+    "revision": "553eb436e0a031b36b26",
+    "url": "/reactcripta/static/js/runtime-main.4aca5172.js"
+  }
+]);
